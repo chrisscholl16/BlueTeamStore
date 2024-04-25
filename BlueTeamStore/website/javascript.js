6c2a4e29e68216ab564cfe6fba58004693f233a0
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-04-24 20:02:14 -0400

    FAQ accordion fix

---
+++ BlueTeamStore/website/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    $(".collapse.show").each(function()
+    {
+        $(this).prev(".card-header").addClass("highlight");
+    });
+    $(".card-header .btn").click(function()
+    {
+    $(".card-header").not($(this).parents()).removeClass("highlight");
+    $(this).parents(".card-header").toggleClass("highlight");
+    });
+});
