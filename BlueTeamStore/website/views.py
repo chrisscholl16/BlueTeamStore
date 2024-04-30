029af9885beec7de1b4bd05fe7fe6094c768f6b7
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-04-30 00:20:39 -0400

    Made item pages for show

--- BlueTeamStore/website/views.py
+++ BlueTeamStore/website/views.py
@@ -29,4 +29,20 @@ def shoes():
 def members():
     return render_template("members.html", user=current_user)
 
+@views.route('/itemMGT.html', methods=['GET', 'POST'])
+def itemMGT():
+    return render_template("itemMGT.html", user=current_user)
+
+@views.route('/itemWBT.html', methods=['GET', 'POST'])
+def itemWBT():
+    return render_template("itemWBT.html", user=current_user)
+
+@views.route('/itemMSK.html', methods=['GET', 'POST'])
+def itemMSK():
+    return render_template("itemMSK.html", user=current_user)
+
+@views.route('/itemWSD.html', methods=['GET', 'POST'])
+def itemWSD():
+    return render_template("itemWSD.html", user=current_user)
+
 
