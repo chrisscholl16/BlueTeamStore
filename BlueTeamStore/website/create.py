78630a34d83be02d0d411b90f422350565648660
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-05-07 12:47:37 -0400

    Had to make some changes to get it to run

--- BlueTeamStore/website/create.py
+++ BlueTeamStore/website/create.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.conncet("user_data.db")
+connection = sqlite3.connect("user_data.db")
 cursor = connection.cursor()
 
 command = """CREATE TABLE IF NOT EXISTS user(username Text, password TEXT)"""
--- BlueTeamStore/website/views.py
+++ BlueTeamStore/website/views.py
@@ -61,24 +61,5 @@ def itemWSD():
     return render_template("itemWSD.html", user=current_user)
 
 
-@views.route('/members.html', methods=['GET', 'POST'])
-def members():
-    return render_template("members.html", user=current_user)
-
-@views.route('/itemMGT.html', methods=['GET', 'POST'])
-def itemMGT():
-    return render_template("itemMGT.html", user=current_user)
-
-@views.route('/itemWBT.html', methods=['GET', 'POST'])
-def itemWBT():
-    return render_template("itemWBT.html", user=current_user)
-
-@views.route('/itemMSK.html', methods=['GET', 'POST'])
-def itemMSK():
-    return render_template("itemMSK.html", user=current_user)
-
-@views.route('/itemWSD.html', methods=['GET', 'POST'])
-def itemWSD():
-    return render_template("itemWSD.html", user=current_user)
 
 
